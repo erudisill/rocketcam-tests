6a093754cf948b40290c04a82e91d6aca66e42a7
Eric Rudisill <erudisill@cphandheld.com>
2015-04-07 14:31:33 -0500

    memory

--- src/main.c
+++ src/main.c
@@ -158,12 +158,88 @@ void init_led(void) {
 	PIOC->PIO_PER = PIO_PER_P12;		// enable pin 12 on PIOC
 }
 
+void init_sram(void) {
+	uint32_t mask = 0;
+
+	// Enable SMC peripheral (10)
+	PMC->PMC_PCER0 = (1 << ID_SMC);
+
+	// Configure SMC peripheral pins: data bus, address bus, NCS0, NRD, NWE
+	// Note: Address bus is spread across PIOC and PIOA
+	mask = 	0xFF 				// data bus D0-D7
+			| PIO_PC8A_NWE 		// NWE
+			| PIO_PC11A_NRD 	// NRD
+			| PIO_PC14A_NCS0	// NCS0
+			| 0xFFFD0000;			// address A0-A13 & A19  (A19?? Datasheet says this is A21 !! pg 52)
+	PIOC->PIO_ABCDSR[0] &= ~mask;	// enable Peripheral A
+	PIOC->PIO_ABCDSR[1] &= ~mask;	// enable Peripheral A
+	PIOC->PIO_PUER = mask;			// enable pullup
+	PIOC->PIO_PDR = mask;			// disable PIO control (enable Peripheral control)
+
+	mask = 0x001C0003;			// address A14-A18
+	PIOA->PIO_ABCDSR[0] &= ~mask;	// enable Peripheral A
+	PIOA->PIO_ABCDSR[1] &= ~mask;	// enable Peripheral A
+	PIOA->PIO_PUER = mask;			// enable pullup
+	PIOA->PIO_PDR = mask;			// disable PIO controll (enable Peripheral control)
+
+	// Configure SRAM_VCC to turn on power to the chip
+	PIOC->PIO_PUDR = PIO_PUDR_P9;	// disable pullup for PC9
+	PIOC->PIO_CODR = PIO_CODR_P9;	// set output data on PC9 to 0
+	PIOC->PIO_OER = PIO_OER_P9;		// enable output on PC9
+	PIOC->PIO_PER = PIO_PER_P9;		// enable PIO control for PC9
+
+	// NOTE: To turn off the SRAM_VCC, PC14 must be disabled and pulled down.
+	//       Otherwise, it feeds the 3V_SRAM net and bypassess the MOSFET switch.
+	//    PIOC->PIO_PER = PIO_PER_P14 ;
+	//    PIOC->PIO_ODR = PIO_ODR_P14 ;
+	//    PIOC->PIO_PUDR = PIO_PUDR_P14 ;
+	//    PIOC->PIO_PPDER = PIO_PPDER_P14 ;
+
+
+	// Configure SMC timing
+	SMC->SMC_CS_NUMBER[0].SMC_SETUP = SMC_SETUP_NWE_SETUP( 2 )
+									 | SMC_SETUP_NCS_WR_SETUP( 0 )
+									 | SMC_SETUP_NRD_SETUP( 3 )
+									 | SMC_SETUP_NCS_RD_SETUP( 0 ) ;
+
+	SMC->SMC_CS_NUMBER[0].SMC_PULSE = SMC_PULSE_NWE_PULSE( 4 )
+									 | SMC_PULSE_NCS_WR_PULSE( 5 )
+									 | SMC_PULSE_NRD_PULSE( 4 )
+									 | SMC_PULSE_NCS_RD_PULSE( 6 ) ;
+
+	SMC->SMC_CS_NUMBER[0].SMC_CYCLE = SMC_CYCLE_NWE_CYCLE( 6 )
+									 | SMC_CYCLE_NRD_CYCLE( 7 ) ;
+
+	SMC->SMC_CS_NUMBER[0].SMC_MODE  = SMC_MODE_READ_MODE
+									 | SMC_MODE_WRITE_MODE;
+
+
+	// TEST TEST TEST TEST
+	//
+
+	#define BOARD_SRAM_BASE           ((void*)0x60000000)
+	#define BOARD_SRAM_LENGTH         ((uint32_t)0x00100000)
+
+	char * pSram = BOARD_SRAM_BASE;
+	for (int i=0;i<0xff;i++) {
+		*(pSram + i + BOARD_SRAM_LENGTH) = i * 2;
+	}
+
+
+
+}
+
+
 int main(void) {
+
+	// Disable Watchdog .. enabled at startup by default - ~16s
+	WDT->WDT_MR = WDT_MR_WDDIS;
 
 	init_clock();
 	init_millis();
 	init_led();
 	init_uart0();
+	init_sram();
 
 	uint32_t prev = 0;
 	uint32_t elapsed = 0;
